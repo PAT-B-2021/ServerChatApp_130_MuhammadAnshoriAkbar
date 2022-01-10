8bf4b31952396f1a636c8fdadeb9d3b134b1e97b
anshoria <anshoriakbar11@gmail.com>
2022-01-10 23:39:34 +0700

    Add project files.

---
+++ ServerChatApp_130_MuhammadAnshoriAkbar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_130;
+using System.ServiceModel;
+
+namespace ServerChatApp_130_MuhammadAnshoriAkbar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostobjek = null;
+            try
+            {
+                hostobjek = new ServiceHost(typeof(ServiceCallback));
+                hostobjek.Open();
+
+                Console.WriteLine("Server On, Ready to Use");
+                Console.ReadLine();
+
+                hostobjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostobjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
